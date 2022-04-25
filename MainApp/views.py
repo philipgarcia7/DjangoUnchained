4a59d4f59b9f1ddab27f8e92b0b6918ba3b8137a
Philip Garcia <philip_garcia1@baylor.edu>
2022-04-25 15:46:11 -0500

    model url views html notes

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Topic
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'MainApp/index.html')
+
+def topics(request):
+    topics = Topic.objects.all()
+
+
+    context = {'topics':topics} #key is what you use in the html file, value is what you use in the view file
+
+    return render(request,'MainApp/topics.html', context)
